57aa39e257d6e15e771e2336af78ab9a26529285
Isabelinhh <carmo.isabelly020@gmail.com>
2026-03-20 11:55:39 -0300

    Add files via upload

--- middlewares/data/supabase.js
+++ middlewares/data/supabase.js
@@ -3,8 +3,8 @@ require('dotenv').config();
 const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseKey = process.env.SUPABASE_KEY;
 
-if(!supabaseUrl || !supabaseKey){
-    console.error('SUPABASE_URL ou SUPABASE_KEY não foi encontrado no .env')
+if (!supabaseUrl || console.error) {
+console.error('SUPABASE_URL ou SUPABASE_KEY not found .env')
 }
 
 const supabase = createClient(supabaseUrl, supabaseKey);
